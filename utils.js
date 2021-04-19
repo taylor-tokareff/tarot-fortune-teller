b350d8fc2fb33ec84634f2eca4d65b3392407e59
taylor-tokareff <taylortokareff@gmail.com>
2021-04-19 14:41:54 -0700

    local storage functions created

---
+++ utils.js
@@ -0,0 +1,13 @@
+
+
+export function saveUser(user) {
+    const saveUserData = JSON.stringify(user);
+    localStorage.setItem('user', saveUserData);
+}
+
+export function getUser() {
+    const getUserData = localStorage.getItem('user');
+    if (!getUserData) return null;
+    const user = JSON.parse(getUserData);
+    return user;
+}
