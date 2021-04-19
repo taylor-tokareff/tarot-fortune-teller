bd804097b72902f51fcb1f6f0baa1b534d1b0d58
taylor-tokareff <taylortokareff@gmail.com>
2021-04-19 11:09:54 -0700

    added to tarot data set

--- data/card-meanings.js
+++ data/card-meanings.js
@@ -1,12 +1,16 @@
-const fool = {
-    id: 0,
+export const tarot = [
 
-    past: "The Fool in the past position indicates that the risks you’ve taken in life to date have set things in motion to soon reward you with great success. By trusting your judgement and following your own path, you’ve set the stage for unlimited potential.",
+    {
+        'name': 'fool',
 
-    present: "In the present position, the Fool indicates that you’re entering a new phase in your life. While this phase could either be positive or negative at the moment, its true influence will be far more apparent to you in the future. Weighing all of your options and assessing risk is paramount to your taking the right decisions.",
+        'id': 0,
 
-    future: "Drawing the Fool in the future position suggests you will soon be embarking on a deeply personal, spiritual journey. You will be setting new goals for yourself and amassing a great wealth of knowledge. That said, to see this realized you must be prepared to shed your old habits in order to fully embrace this new direction in your life.",
-}
+        'past': "The Fool in the past position indicates that the risks you’ve taken in life to date have set things in motion to soon reward you with great success. By trusting your judgement and following your own path, you’ve set the stage for unlimited potential.",
+
+        'present': "In the present position, the Fool indicates that you’re entering a new phase in your life. While this phase could either be positive or negative at the moment, its true influence will be far more apparent to you in the future. Weighing all of your options and assessing risk is paramount to your taking the right decisions.",
+
+        'future': "Drawing the Fool in the future position suggests you will soon be embarking on a deeply personal, spiritual journey. You will be setting new goals for yourself and amassing a great wealth of knowledge. That said, to see this realized you must be prepared to shed your old habits in order to fully embrace this new direction in your life.",
+    },
 
 const magician = {
     id: 1,
@@ -16,7 +20,7 @@ const magician = {
     present: "A recent event makes you discover your spiritual potential. This is a great time to do something that you have thought about in the past but did not take the steps to pursue. You are open and have an attentive ear to the inner voice that will guide you. It may lead you to looking for spiritual counsel, joining a group or following a spiritual guide.",
 
     future: "If a recent event has temporarily made it difficult for you to use the spiritual resources you have at hand, you may be inclined to use dangerous occult forces in order to reach your spiritual goals. Be wary of con artists of the esoteric.",
-}
+},
 
 const high_priestess = {
     id: 2,
@@ -26,7 +30,7 @@ const high_priestess = {
     present: "When the High Priestess is in the present position, you’re advised to never believe what’s presented as the truth. The skills you developed in the past will apply here, making it easier for you to avoid bad a situation. Is your partner insisting on you moving in together, despite your objections? Do you sense that someone walking down the street is following you too closely? Follow these hunches — you’ll be grateful that you did.",
 
     future: "In the future position, the High Priestess means that you’re headed towards a conflict that should be defused or avoided altogether. Use your foresight to guide you to the best outcome. This card is one that knows without knowing — use that ability to better your future.",
-}
+},
 
 const empress = {
     id: 3,
@@ -36,7 +40,7 @@ const empress = {
     present: "In the present, the Empress is a sign that you need to be sensitive to the emotional needs and well-being of people around you. Like it or not, you are seen as a savior of sorts to these people and have no choice but to be supportive and genuinely empathetic to their concerns. A new beginning may be awaiting you.",
 
     future: "In the future position, the Empress indicates that a much needed period of emotional and possible physical healing awaits you. Be open to new experiences. If you play your cards right, all that effort which has left you weak and emotionally exhausted will eventually pay off.",
-}
+},
 
 const emperor = {
     id: 4,
@@ -46,7 +50,7 @@ const emperor = {
     present: "In the present position, it means that there’s a good chance you’re about to be introduced to a celebrity or famous person who was one of your childhood heroes. At the same time, there is an individual in your life who is introducing you to a new way of being, a more positive approach that this person, likely an older, powerful male, personifies.",
 
     future: "In the future position the Emperor tarot card indicates that you are developing a new, more conservative mindset that will eventually have an impact on your political leanings, which in turn could negatively affect several of your personal relationships. A powerful male figure will be the impetus behind your apparent political awakening. However, this person is not to be trusted and will prove to be a negative force whose teachings only bring you, along with the people who love you, harm.",
-}
+},
 
 const hierophant = {
     id: 5,
@@ -56,7 +60,7 @@ const hierophant = {
     present: "As an indicator of your present situation, the Hierophant represents a new way of living and belief system that could be taking a hold and motivating you in a positive manner. However, you need to move slowly and carefully, don’t dive into any new venture without first giving it your full consideration.",
 
     future: "In the future position, the Hierophant is suggesting that you don’t need to do anything special to plan for your future right now. Everything is as it should be and will organically unfold in a positive fashion.",
-}
+},
 
 const lovers = {
     id: 6,
@@ -66,7 +70,7 @@ const lovers = {
     present: "In the present position, the Lovers card means that a relationship is developing and will bring passion and happiness. It can also indicate that a period of loneliness is about to come to an end, possibly with a new love or with personal growth and acceptance. In either case, the situation will be a transformative one.",
 
     future: "In the future position, the Lovers card indicates that the time is right to seek out love, while maintaining a clear understanding of your expectations. Don’t settle for less, and don’t be afraid to go for what you want. If you are in a relationship, communicate your needs to your partner. This card is a sign that their response will be positive.",
-}
+},
 
 const chariot = {
     id: 7,
@@ -76,17 +80,17 @@ const chariot = {
     present: "In the present position, the Chariot means that you are about to enjoy the fruits of your labor. Sometimes, getting everything that you want can be a curse in itself, yet more often than not your reward is something welcomed with open arms.",
 
     future: "In the future position, the Chariot indicates that you should be filled with energy knowing that the trials you are currently facing, or have yet to meet, will be overcome. Success awaits you beyond your struggle, and it is yours for the taking.",
-}
+},
 
 const strength = {
-    id: 8, 
+    id: 8,
 
     past: "The past position for Strength is a particularly interesting state. This often refers to your composure in the face of adversity leading to your current success. In some cases, Strength in the past position refers to your ability to consistently seek new challenges, never settling into a single position for too long. Essentially, Strength in the past signifies constant transition and victory through your iron will to hold it all together.",
 
     present: "In the present position, the Strength card is a challenge to take on more. Do not turn down opportunities simply because they are risky or difficult. The odds are in your favor, and now is the time invest yourself in uncertain ventures that you are passionate about.",
 
     future: "In the future position, Strength is an encouragement, promising that your current troubles will soon be under control, and might even contribute to your success.",
-}
+},
 
 const hermit = {
 
@@ -97,7 +101,7 @@ const hermit = {
     present: "The Hermit in the present position means that you are going through a period of solitude and contemplation. This could be a sign of a recent breakup or that you are feeling alone in a relationship.",
 
     future: "If you’re in a relationship and you draw the Hermit in the future position, be warned that a separation is likely. This may be a blessing in disguise, though, as it can mark the end of a destructive relationship or the beginning of a period of personal growth.",
-}
+},
 
 const wheelOfFortune = {
     id: 10,
@@ -107,7 +111,7 @@ const wheelOfFortune = {
     present: "In the present position, the Wheel of Fortune means that chance is on your side. Good things are coming your way, although they may first appear as obstacles. Don’t wait too long or the opportunity will pass you by.",
 
     future: "When found in the future position, the Wheel of Fortune represents an impending change. This change can be positive or negative — all will depend on your internal strength and ability for self-understanding.",
-}
+},
 
 const justice = {
     id: 11,
@@ -117,7 +121,7 @@ const justice = {
     present: "When Justice is in the present position, it is a sign that you are at an impasse. You will not be able to make this decision alone — you must ask for advice, even if that makes you feel vulnerable. These choices will set the foundations for many years to come.",
 
     future: "Drawn in the future position, Justice means that any past wrongs you have suffered will be righted. Those who have hurt you will face justice, and you will emerge victorious with the respect and admiration of your community.",
-}
+},
 
 const justice = {
     id: 12,
@@ -127,7 +131,7 @@ const justice = {
     present: "When Justice is in the present position, it is a sign that you are at an impasse. You will not be able to make this decision alone — you must ask for advice, even if that makes you feel vulnerable. These choices will set the foundations for many years to come.",
 
     future: "Drawn in the future position, Justice means that any past wrongs you have suffered will be righted. Those who have hurt you will face justice, and you will emerge victorious with the respect and admiration of your community.",
-}
+},
 
 const hangedMan = {
     id: 13,
@@ -137,7 +141,7 @@ const hangedMan = {
     present: "In the past position, the Hanged Man card indicates that you’ve finally managed to let go of the lingering negative memories you’ve been harboring all these years, and that this has proven beneficial to you spiritually. You can now safely look back at these experiences from a fresh perspective.",
 
     future: "In the future position, the Hanged Man tells you that things remain uncertain, so be sure to plan your next moves carefully.  If you’ve been hurried and rushing lately, you should relax and give pause to the situations around you.",
-}
+},
 
 const death = {
     id: 14,
@@ -147,7 +151,7 @@ const death = {
     present: "In the present position, it means that something big is literally in the cards for you and it will arrive with little to no advance warning. This is likely to be a positive change, although you may lose something that’s been important to you in the process.",
 
     future: "Death in the future position is a sign that you need to find a new perspective on the world in order to move forward. Put the past behind you, your journey is just beginning.",
-}
+},
 
 const temperance = {
     id: 15,
@@ -157,7 +161,7 @@ const temperance = {
     present: "In the present position, Temperance means that something you’ve long yearned for — be it wealth, romance, a new car or something entirely different — is about to come to fruition.",
 
     future: "In the future position, Temperance warns you that in order to resolve any current conflicts and move forward in life, you need to stay in control of your life by exercising restraint, balance, and moderation.",
-}
+},
 
 const devil = {
     id: 16,
@@ -167,7 +171,7 @@ const devil = {
     present: "In the present position, the Devil represents something over which you have no control that is currently happening in your life. It remains to be seen if this change will be for the better. Avoid reckless behavior and do your very best to live with integrity right now.",
 
     future: "The Devil card in the future position announces that a major reckoning is coming your way. When it arrives you will need to finally accept yourself for who you are, and not the mask you prefer to show the world.",
-}
+},
 
 const tower = {
     id: 17,
@@ -177,7 +181,7 @@ const tower = {
     present: "In the present position, it represents a longstanding conflict that’s about to come to its conclusion. It could be of a romantic nature, and if so, you need to step back and reevaluate the relationship in order to proceed in the right direction.",
 
     future: "The Tower indicates that the worst is behind you, but you still have a series of major challenges coming your way. So long as you take on and survive these challenges gracefully, your immediate future promises to be bright.",
-}
+},
 
 const star = {
     id: 18,
@@ -187,7 +191,7 @@ const star = {
     present: "In the present position, the Star means that you are about to make a major life decision. You may be overly trusting or unknowingly making risky moves. Speak with a close friend or relative for advice. You likely feel idealistic and ready to follow your dreams. This motivation can be the push that you need to make your aspirations a reality. Overall, the Star card in the present position calls on you to seek advice, but also to believe in yourself.",
 
     future: "In the future position, the Star indicates that you are at a relative low point in your journey. Light is on the horizon — you simply need to stay positive. Luck is on your side, and making bold moves now will pay off later. The future is bright, your day is soon to come.",
-}
+},
 
 const moon = {
     id: 19,
@@ -197,7 +201,7 @@ const moon = {
     present: "The Moon is a clear sign of change. You are or will be uncertain about how to proceed, but this situation will be less foreign to you than it may seem. Look to the past for guidance — you already have the experience you need to overcome this challenge if you are able to recognize it for what it is.",
 
     future: "The Moon is a clear sign of change. You are or will be uncertain about how to proceed, but this situation will be less foreign to you than it may seem. Look to the past for guidance — you already have the experience you need to overcome this challenge if you are able to recognize it for what it is.",
-}
+},
 
 const sun = {
     id: 20,
@@ -207,24 +211,30 @@ const sun = {
     present: "The Sun in the present position means that you’re the center of attention, either at work or within your social entourage. All eyes are on you, so try to keep your feet on the ground — enjoy the attention, but remain modest and kind.",
 
     future: "The Sun in the future position suggests that your current efforts will likely be fruitful, so don’t give up on them. You may notice certain situations turning out even better than you originally expected.",
-}
+},
 
-const judgement = {
-    id: 21,
+    {
+        'name': 'judgement',
 
-    past: "In a past position, Judgement indicates that your current life is the end result of a single major decision that you made in the past. Day to day living obliges all of us to make decisions both large and small, but you made one decision that left an indelible mark on your life, for better or worse. Perhaps you chose to move away from home in college or drop out of school. Maybe you got caught stealing, or found the love of your life on a blind date. It can be hard to think of your life unfolding any other way than it has, but if you had made this one choice differently, your own life would be unrecognizable to you.",
+        'id': 21,
 
-    present: "In your present situation, Judgement indicates that you are thinking about an important decision. While the Judgement card does not necessarily offer guidance on which choice is correct, it does emphasize that the decision you are reflecting on is in fact life altering. That said, Judgement tends to favor change, meaning the right decision tends to be the one that takes you most out of your comfort zone.",
+        'img': './images/main-deck/20-judgement.jpg',
 
-    future: "As it concerns the future, Judgement is the light at the end of a long tunnel. Your current confusion and the conflicts that are taking up too much of your time will come to a head at some future point. A clear choice will present itself, giving you the opportunity to simplify your life.",
-}
+        'past': "In a past position, Judgement indicates that your current life is the end result of a single major decision that you made in the past. Day to day living obliges all of us to make decisions both large and small, but you made one decision that left an indelible mark on your life, for better or worse. Perhaps you chose to move away from home in college or drop out of school. Maybe you got caught stealing, or found the love of your life on a blind date. It can be hard to think of your life unfolding any other way than it has, but if you had made this one choice differently, your own life would be unrecognizable to you.",
+
+        'present': "In your present situation, Judgement indicates that you are thinking about an important decision. While the Judgement card does not necessarily offer guidance on which choice is correct, it does emphasize that the decision you are reflecting on is in fact life altering. That said, Judgement tends to favor change, meaning the right decision tends to be the one that takes you most out of your comfort zone.",
+
+        'future': "As it concerns the future, Judgement is the light at the end of a long tunnel. Your current confusion and the conflicts that are taking up too much of your time will come to a head at some future point. A clear choice will present itself, giving you the opportunity to simplify your life.",
+    },
 
 const world = {
     id: 22,
-    
+
     past: "In the past position, the World indicates that your path will lead you full circle but grant you a deeper understanding of your situation. Perhaps you will move to a faraway city only to move back home thereafter, or leave your job for a new opportunity and return in the long run. This shouldn’t be seen as a bad sign — your return will be wholly voluntary and triumphant.",
 
     present: "In the present position, the World is a good omen and signifies that you’re on the path to success. Stick with the plan and you will be rewarded.",
 
     future: "In the future position, the World is a sign of uncertainty. Until now, you’ve been living without a clear goal or direction. Defining success can often be a difficult process, but with a clear endpoint you will be able to find your way to success and happiness.",
 }
+
+]
